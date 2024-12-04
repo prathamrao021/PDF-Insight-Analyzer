0990e3d5b5360042bdd95f6608e9a35304698367
prathamrao021 <prathamrao021@gmail.com>
2024-12-04 16:51:36 -0500

    arranged the directories

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # cis6930fa24-project3
+This project is a web application that allows users to upload PDF files or provide URLs to PDF files, processes the data, and visualizes the clustering results using various charts. The application is built using Next.js for the frontend and FastAPI for the backend.
+
+
+## Features
+
+- Upload PDF files or provide URLs to PDF files.
+- Process the uploaded files or URLs to extract data.
+- Visualize the clustering results using 2D and 3D scatter plots and bar charts.
+- Customize the legend position and appearance in the charts.
+
+## Technologies Used
+
+- **Frontend:** Next.js, React, react-plotly.js, react-chartjs-2
+- **Backend:** FastAPI, scikit-learn, pandas, requests
+- **Other:** Plotly, Chart.js
+
+## Installation
+
+### Prerequisites
+
+- Node.js and npm
+- Python and pip
+
+### Backend Setup
+
+1. **Clone the repository:**
+   ```sh
+   git clone https://github.com/prathamrao021/cis6930fa24-project3
+   cd cis6930fa24-project3
+
+2. **Install pipenv if you haven't already:**
+    ```sh
+    pip install pipenv
+
+3. **Create a virtual environment and install dependencies:**
+    ```sh
+    pipenv install
+
+4. **Activate the virtual enviornment:**
+    ```sh
+    python -m pipenv shell
+
+5. **Run the FastAPI server:**
+    ```sh
+    pipenv run fastapi dev main.py
+
+### Frontend Setup
+
+1. **Navigate to the frontend directory:**
+    ```sh
+    cd src
+    cd frontedn
+
+2. **Install the required npm packages:**
+    ```sh
+    npm install
+
+3. **Run the Next.js development server:**
+    ```sh
+    npm run dev
+
+### Usage
+1. Open your browser and navigate to `http://localhost:3000`.
+2. Use the form to upload PDF files or provide URLs to PDF files.
+3. Click the "Upload" button to process the files or URLs.
+4. View the clustering visualizations in the charts.
+
+## Functions
+## Video
+## Bugs and Assumptions
---
+++ src/backend/process_data.py
@@ -0,0 +1,125 @@
+from pypdf import PdfReader
+import pandas as pd
+import os
+import glob
+# import requests
+import time
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.cluster import KMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+API_KEY = "..."
+
+def fetchincidents(pdf):
+    reader = PdfReader(pdf)
+    data = ''
+    for page in reader.pages:
+        data = data + "\n" + page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False)
+
+    return data
+
+
+def extractincidents(data):
+
+    lines = (data.split("\n"))[4:-1]
+    rows = []
+
+    temp_row = []
+    temp_text = ""
+    for line in lines:
+        split_line = [field.strip() for field in line.split("          ") if field.strip()]
+
+        if len(split_line) < 5:
+            if len(temp_row) > 0:
+                temp_row[2] += " " + split_line[0]
+                
+        else:
+            if len(temp_row) == 5:
+                temp_text += "\t".join(temp_row)
+                temp_text += "\n"
+                rows.append(temp_row)
+            temp_row = split_line 
+
+    if len(temp_row) == 5:
+        rows.append(temp_row)
+        temp_text += "\t".join(temp_row)
+
+    if not os.path.exists('src/backend/resources/data.tsv'):
+        with open('src/backend/resources/data.tsv', 'w') as file:
+            file.write(temp_text)
+
+    # Write the extracted data to a file
+    with open('src/backend/resources/data.tsv', 'a') as file:
+        file.write(temp_text)
+    
+    return rows
+
+def convert_data_to_numeric(data):
+    data = pd.DataFrame(data, columns=["Date/Time", "IncidentNumber", "Location", "Nature", "IncidentOri"])
+    
+    # try:
+    #     data['Latitude'], data['Longitude'] = zip(*data['Location'].apply(get_lat_lng))
+    # except:
+    #     data['Latitude'], data['Longitude'] = 0, 0
+
+    data['Hour'] = pd.to_datetime(data['Date/Time']).dt.hour
+    data['Day'] = pd.to_datetime(data['Date/Time']).dt.day
+    
+    # missing_locations = data[data[['Latitude', 'Longitude']].isna().any(axis=1)]
+
+    encoder = LabelEncoder()
+    data['NatureEncoded'] = encoder.fit_transform(data['Nature'])
+    data['Day'] = encoder.fit_transform(data['Day'])
+
+    data = data.drop(columns=['Date/Time', 'Location', 'Nature', 'IncidentNumber', 'IncidentOri'])
+
+    # print(data)
+
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
+
+    return data
+
+def write_processded_data(data):
+    data.to_csv('resources/processed_data.csv', index=False)
+
+def pca_conversion(data):
+    pca = PCA(n_components=2)  # Reduce to 2 components for visualization
+    principal_components = pca.fit_transform(data.drop(columns=['Cluster']))
+    pca_data = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
+    pca_data['Cluster'] = data['Cluster']
+    return pca_data
+
+def pca_conversion3(data):
+    pca = PCA(n_components=3)  # Reduce to 2 components for visualization
+    principal_components = pca.fit_transform(data.drop(columns=['Cluster']))
+    pca_data = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
+    pca_data['Cluster'] = data['Cluster']
+    return pca_data
+
+#how the workflow will look like
+def main_workflow():
+    pdf_files = glob.glob(os.path.join('uploads', '*.pdf'))
+
+    combined_data = pd.DataFrame()
+    # clusters = pd.DataFrame()
+    for pdf_file in pdf_files:
+        incidents = fetchincidents(pdf_file)
+        separated_data = extractincidents(incidents)
+        encoded_data = convert_data_to_numeric(separated_data)
+        combined_data = pd.concat([combined_data, pd.DataFrame(encoded_data)], ignore_index=True)
+
+    kmeans = KMeans(n_clusters=5, random_state=42)
+    combined_data['Cluster'] = kmeans.fit_predict(combined_data)
+
+    
+    write_processded_data(combined_data)
+
+    combined_linear_data = pca_conversion(combined_data)
+
+
+# if __name__ == "__main__":
+#     main_workflow()
